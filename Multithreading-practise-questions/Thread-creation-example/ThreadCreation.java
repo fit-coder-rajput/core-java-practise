4f82e8bb91f49120b341387272eeaa507860e9f8
fit-coder-rajput <itsdhruvrajput@gmail.com>
2024-08-23 20:08:53 +0530

    folder name changed

---
+++ Multithreading-practise-questions/Thread-creation-example/ThreadCreation.java
@@ -0,0 +1,27 @@
+
+public class ThreadCreation {
+
+	public static void main(String[] args) {
+
+		Thread thread1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				System.out.println("First thread created my name is : " + Thread.currentThread().getName());
+			}
+		});
+
+		Thread thread2 = new Thread(() -> { // using lambda we can reduce to this
+
+			System.out.println("Second thread created my name is : " + Thread.currentThread().getName());
+		});
+
+		System.out.println("Before thread start current thread is : " + Thread.currentThread().getName());
+		thread1.start();
+		thread2.start();
+		System.out.println("after thread start current thread is : " + Thread.currentThread().getName());
+
+	}
+
+}
